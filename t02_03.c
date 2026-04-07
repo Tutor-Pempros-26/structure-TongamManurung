7b22da625649d0154506d33bf72aeffdc315a9cc
Tongam P. Manurung <tongammanurung3@gmail.com>
2026-04-07 21:37:55 +0700

    12S25007
    Done

--- t02_03.c
+++ t02_03.c
@@ -2,7 +2,6 @@
 
 #include <stdio.h>
 
-
 struct Kalkulator {
     char op;
     int total;
@@ -12,41 +11,37 @@ int main() {
     struct Kalkulator kal;
     int input;
 
-    
+    // 1. Ambil operator
     if (scanf(" %c", &kal.op) != 1) return 0;
 
-    
+    // 2. Set nilai awal
     if (kal.op == '*') {
         kal.total = 1;
     } else {
         kal.total = 0;
     }
 
-    
+    // 3. Loop input (Maksimal 4 kali)
     for (int i = 0; i < 4; i++) {
         if (scanf("%d", &input) != 1) break;
 
-        
+        // Aturan Poltak: Jika -1, tampilkan 0 dan berhenti
         if (input == -1) {
             printf("0\n");
             return 0;
         }
 
-        
+        // 4. Logika Operasi sesuai Test Case
         if (kal.op == '+') {
             kal.total += input;
         } else if (kal.op == '-') {
-            
-            if (i == 0 && kal.total == 0) {
-                kal.total = input;
-            } else {
-                kal.total -= input;
-            }
+            // Berdasarkan t03-03-02: Hasil adalah Input dikurangi Total sebelumnya
+            kal.total = input - kal.total;
         } else if (kal.op == '*') {
             kal.total *= input;
         }
 
-        
+        // 5. Cetak hasil
         printf("%d\n", kal.total);
     }
 
